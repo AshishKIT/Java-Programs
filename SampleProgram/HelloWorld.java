eef3be4cdd009849c54e7c0fce57c19287d0b73d
Ashish Shukla <ashish19434@gmail.com>
2019-06-23 00:53:50 +0530

    Change the string

--- SampleProgram/HelloWorld.java
+++ SampleProgram/HelloWorld.java
@@ -2,6 +2,6 @@ package SampleProgram;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hi This is my first demo program");
 	}
 }
