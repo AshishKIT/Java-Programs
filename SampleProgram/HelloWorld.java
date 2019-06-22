582e331af23e0aac720a840763734d34f54bded5
Rajneesh Shukla <rajneesh9651@gmail.com>
2019-06-23 00:57:29 +0530

    Again change string

--- SampleProgram/HelloWorld.java
+++ SampleProgram/HelloWorld.java
@@ -2,6 +2,6 @@ package SampleProgram;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hi This is my first demo program");
+		System.out.println("This is Rajneesh");
 	}
 }
